998b8825cf4fec6774417b9593f868dc478002f7
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2023-01-21 16:55:44 +0200

    chore: .gitignore

--- src/Function/Multer/Multer.js
+++ src/Function/Multer/Multer.js
@@ -1,3 +1,8 @@
 import multer from "multer";
 import path from "path";
 
+const multerStorage = multer.diskStorage({
+    destination: (cb) => {
+        cb(null, path.join(__dirname,'/uploads/'))
+    }
+})
